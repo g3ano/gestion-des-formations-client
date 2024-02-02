cb1bc140f4790270b294e873d8d73a3d8eef3f40
ghano <ghanoasx@gmail.com>
2024-02-02 01:14:43 +0100

    refactor: replace radix-icon with lucid-icons

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Cross2Icon } from '@radix-ui/react-icons';
 import * as ToastPrimitives from '@radix-ui/react-toast';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import Icon from '@/components/ui/icon';
+import { X } from 'lucide-react';
 
 const ToastProvider = ToastPrimitives.Provider;
 
@@ -81,7 +82,7 @@ const ToastClose = React.forwardRef<
     toast-close=''
     {...props}
   >
-    <Cross2Icon className='h-4 w-4' />
+    <Icon render={X} />
   </ToastPrimitives.Close>
 ));
 ToastClose.displayName = ToastPrimitives.Close.displayName;
